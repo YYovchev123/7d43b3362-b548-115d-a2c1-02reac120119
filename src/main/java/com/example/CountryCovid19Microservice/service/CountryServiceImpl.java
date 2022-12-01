e5523ee82d65f2527650332e66e0589f623aafbe
YYovchev123 <102542112+YYovchev123@users.noreply.github.com>
2022-12-01 22:31:02 +0200

    first commit

--- src/main/java/com/example/CountryCovid19Microservice/service/CountryServiceImpl.java
+++ src/main/java/com/example/CountryCovid19Microservice/service/CountryServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CountryServiceImpl implements CountryService{
-
     private CountryRepository countryRepository;
 
     @Override
